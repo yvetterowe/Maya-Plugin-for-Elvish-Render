5e1c112b3f306f22efcf95ed3ca299d00f2b3e4f
yvetterowe1116@gmail.com <yvetterowe1116@gmail.com@f82b52a9-fb2f-50e7-dc0d-a1cbf65dcd3b>
2011-12-03 05:24:08 +0000

    fix export polygon bugs =D

--- MayaExporterForER/MayaExporterForER/DagNodeWriter.cpp
+++ MayaExporterForER/MayaExporterForER/DagNodeWriter.cpp
@@ -9,3 +9,10 @@ DagNodeWriter::~DagNodeWriter()
 {
 	if(fpath!=NULL) delete fpath;
 }
+
+void DagNodeWriter::outputTabs( ostream & os, int tabCount )
+{
+	for (int i = 0; i < tabCount; i++) {
+		os << "\t";
+	}
+}
