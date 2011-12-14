ae1f66a559d9af8a7472f0a913c2d8c35e34525a
yvetterowe1116@gmail.com <yvetterowe1116@gmail.com@f82b52a9-fb2f-50e7-dc0d-a1cbf65dcd3b>
2011-12-14 17:22:54 +0000

    implement ui control & render bmp in viewport ! congrats =)

--- MayaExporterForER/MayaExporterForER/MayaExporterForER.h
+++ MayaExporterForER/MayaExporterForER/MayaExporterForER.h
@@ -2,6 +2,9 @@
 
 #include <maya/MPxFileTranslator.h>
 #include <maya/MStringArray.h>
+#include <maya/MArgList.h>
+
+#include "OptionType.h"
 
 class MDagPath;
 class MFnDagNode;
@@ -21,6 +24,18 @@ public:
 	virtual bool            canBeOpened () const;
 	MString                 defaultExtension() const;
 
+	void					parseArglist( const MArgList& args );
+
+	//get options
+	OpContrast				getContrast()			{ return opContrast;}
+	OpSample				getSample()				{ return opSample;}
+	OpFilter				getFilter()				{ return opFilter;}
+	OpTraceDepth			getTraceDepth()			{ return opTraceDepth;}
+	int						getGloabalIllumi()		{ return opGlobalIllumi;}
+	int						getFinalGather()		{ return opFinalGather;}
+	OpResolution			getResolution()			{ return opResolution;}
+
+
 private: 
 	virtual bool            isVisible(MFnDagNode& fnDag, MStatus& status);
 	virtual MStatus         exportAll(ostream& os);
@@ -30,7 +45,24 @@ private:
 	void					outputRenderConfig(ostream& os);
 	void                    outputOptions(ostream& os);
 
+	//set options
+	void					setContrast(double r,double g,double b,double a);
+	void					setSample(int sMin,int sMax);
+	void					setFilter(MString t,int s);
+	void					setTraceDepth(int tRl,int tRR,int tSum);
+	void					setGlobalIllumi(int g);
+	void					setFinalGather(int f);
+
 	MStringArray			instanceContainer;
 	MString                 camaraInstance;
 	MString					option;
+
+	//options
+	OpContrast				opContrast;
+	OpSample				opSample;
+	OpFilter				opFilter;
+	OpTraceDepth			opTraceDepth;
+	int						opGlobalIllumi;
+	int						opFinalGather;
+	OpResolution			opResolution;
 };
